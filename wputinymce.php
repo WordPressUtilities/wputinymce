7e577e72e0b4c7ca1d73c40ad877326b3e2e6c8f
Kevin <kevin@rocher-dev.com>
2019-07-22 16:02:25 +0200

    v 0.10.2
    * Fix invalid var.
    * Add updater.

--- wputinymce.php
+++ wputinymce.php
@@ -4,7 +4,7 @@
 Plugin Name: WPU TinyMCE Buttons
 Plugin URI: http://github.com/Darklg/WPUtilities
 Description: Add new buttons to TinyMCE
-Version: 0.10.1
+Version: 0.10.2
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -12,8 +12,11 @@ License URI: http://opensource.org/licenses/MIT
 */
 
 class WPUTinyMCE {
-    public $plugin_version = '0.10.1';
+    public $plugin_version = '0.10.2';
     private $files_length = false;
+    private $options = array(
+        'plugin-id' => 'wpu_tinymce'
+    );
 
     public function __construct() {
 
@@ -22,6 +25,9 @@ class WPUTinyMCE {
         $this->up_url = $upload_dir['baseurl'] . '/wpu_tinymce-cache';
         $this->plugin_assets_dir = dirname(__FILE__) . '/assets/';
 
+        add_action('init', array(&$this,
+            'check_update'
+        ));
         add_action('init', array(&$this,
             'check_buttons_list'
         ));
@@ -46,6 +52,14 @@ class WPUTinyMCE {
         add_action('admin_enqueue_scripts', array(&$this,
             'load_assets'
         ));
+    }
+
+    public function check_update() {
+        include dirname(__FILE__) . '/inc/WPUBaseUpdate/WPUBaseUpdate.php';
+        $this->settings_update = new \wputinymce\WPUBaseUpdate(
+            'WordPressUtilities',
+            'wputinymce',
+            $this->plugin_version);
     }
 
     public function check_buttons_list() {
@@ -181,11 +195,8 @@ class WPUTinyMCE {
     }
 
     public function set_options() {
-        $this->options = array(
-            'plugin-id' => 'wpu_tinymce',
-            'buttons' => $this->buttons,
-            'quicktags' => $this->quicktags
-        );
+        $this->options['buttons'] = $this->buttons;
+        $this->options['quicktags'] = $this->quicktags;
     }
 
     public function load_assets() {
